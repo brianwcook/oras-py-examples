f4790d85c92c673fa54c194c776ead56110db091
Brian Cook <bcook@redhat.com>
2024-06-01 09:57:35 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import oras.client
 
 client = oras.client.OrasClient()
-client.push(files=["random.txt"], target="quay.io/bcook/tuneablechunk:2", do_chunked=True, chunk_size=83886080)
+client.push(files=["random.txt"], target="quay.io/bcook/tuneablechunks-b:1", do_chunked=True, chunk_size=10000000)
 
 # Successfully pushed localhost:5000/dinosaur/artifact:v1
-# Out[4]: <Response [201]>
