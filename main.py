7f7ec1205d572d1eb9751d0c0f95b1b234bc7b9d
Brian Cook <bcook@redhat.com>
2024-06-03 17:59:29 -0400

    print file size at start

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import oras.client
+import os
+
+file_name="disk.qcow2.gz"
+file_stats=os.stat(file_name)
+file_size=file_stats.st_size
+print("file size:", str(file_size))
 
 client = oras.client.OrasClient()
-client.push(files=["random.txt"], target="quay.io/bcook/tuneablechunks-b:1", do_chunked=True, chunk_size=10000000)
+client.push(files=["disk.qcow2.gz"], target="quay.io/bcook/tuneablechunks-b:2", do_chunked=True, chunk_size=100000000)
 
-# Successfully pushed localhost:5000/dinosaur/artifact:v1
