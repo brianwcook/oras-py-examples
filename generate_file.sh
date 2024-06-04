c4dcaf32e2decd922d34d330c644d477e571e7d1
Brian Cook <bcook@redhat.com>
2024-06-04 16:32:06 -0400

    add argparse

--- generate_file.sh
+++ generate_file.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # 1 gigabyte is usually 2**30 bytes (though you can use 10**9 for 109 bytes instead). The * 3/4 part accounts for Base64 overhead, making the encoded output 1 GB.
 
-openssl rand -out random.txt -base64 $(( 2**27 * 3/4 ))
+openssl rand -out random.txt -base64 $(( 2**30 * 3/4 * 25 )) # generate 25gb file
--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import oras.client
 import os
+import argparse
+import oras.utils
 
-file_name="disk.qcow2.gz"
-file_stats=os.stat(file_name)
-file_size=file_stats.st_size
-print("file size:", str(file_size))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--file', dest='file_name', type=str, help='file to upload')
+parser.add_argument('--config', dest='manifest_config', type=str, help='manifest config')
+parser.add_argument('--target', dest='target', type=str, help='where to store the file')
+
+args = parser.parse_args()
+
+path = args.file_name.split('/')[0:-1]
+path = "/".join(path)
+file = args.file_name.split('/')[-1]
+print(path, file)
+os.chdir(path)
 
 client = oras.client.OrasClient()
-client.push(files=["disk.qcow2.gz"], target="quay.io/bcook/tuneablechunks-b:3", do_chunked=True, chunk_size=1000000000)
+client.push(files=[file], target=args.target, do_chunked=True, chunk_size=1000000000)
 
