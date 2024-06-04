92fd32ea03cdfeb8151a514bc583a45323e69c15
Brian Cook <bcook@redhat.com>
2024-06-03 20:00:18 -0400

    1GB chunk sizes

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ file_size=file_stats.st_size
 print("file size:", str(file_size))
 
 client = oras.client.OrasClient()
-client.push(files=["disk.qcow2.gz"], target="quay.io/bcook/tuneablechunks-b:2", do_chunked=True, chunk_size=100000000)
+client.push(files=["disk.qcow2.gz"], target="quay.io/bcook/tuneablechunks-b:3", do_chunked=True, chunk_size=1000000000)
 
